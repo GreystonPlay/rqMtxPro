673c53d92c0eae5457174f086d132e0336a8fe94
GreystonPlay <greystonbot1@gmail.com>
2021-11-17 01:53:13 +0300

    tst rename

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace rqMtx.Properties {
+namespace rqMtxLite.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace rqMtx.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("rqMtx.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("rqMtxLite.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
